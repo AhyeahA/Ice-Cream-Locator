d23545c9275d66d35199ea86a265882df0a97ed7
Aiyh <abuzouraiyh@gmail.com>
2021-09-23 17:40:34 -0500

    Secret key hidden

--- Ice Cream Locator App/icecreamsite/icecreamsite/settings.py
+++ Ice Cream Locator App/icecreamsite/icecreamsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t*t&(26uq5a3x9%3o+a+ntcic9+@9dar5g%ix&@8=b$41nhe1f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
